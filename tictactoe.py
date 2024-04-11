2ba85708afc5d4ab4f89754d6d1726f1b29402cb
Ben <jbensmyth@gmail.com>
2024-04-11 13:41:38 -0400

    Update imports

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,16 @@
-from computer import Computer
 from game import Game
 from pieces import O, X
-from player import Player
+from player import Computer, Player
+
+game = Game(
+  p1=Player(X),
+  p2=Computer(O),
+  width=3,
+  limit=9,
+  prune=True,
+  order=False,
+  utype=1)
 
 print("\nTic-Tac-Toe\n")
-p1 = Player(X)
-p2 = Computer(O)
-game = Game(p1, p2, width=3, limit=9, prune=True, order=False, utype=1)
 winner = game.play()
 print("Tie" if not winner else f"Winner {winner}")
