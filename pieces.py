f3b0d06e625b1d92024dc4f4822290c311c0bea0
Ben <jbensmyth@gmail.com>
2024-04-27 21:46:53 -0400

    Clean code

--- pieces.py
+++ pieces.py
@@ -9,10 +9,11 @@ class Piece():
     def set(self, cell):
         self.cell = cell
 
-class X(Piece):
-    def __init__(self, cell=None):
-        super().__init__('X', cell)
 
 class O(Piece):
     def __init__(self, cell=None):
         super().__init__('O', cell)
+
+class X(Piece):
+    def __init__(self, cell=None):
+        super().__init__('X', cell)
