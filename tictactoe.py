f3c0b985ee23f4d432d3c515a52fcf3c25aea95e
Ben <jbensmyth@gmail.com>
2024-05-10 05:18:55 -0400

    Simplify options code

--- tictactoe.py
+++ tictactoe.py
@@ -3,21 +3,16 @@ from pieces import O, X
 from player import Computer, Player
 from sys import argv
 
-WIDTH = 3
-EXPONENT = 2
-
-try:
-  WIDTH = int(argv[1])
-  EXPONENT = float(argv[2])
-except IndexError: ...
+try: (WIDTH := int(argv[1]))
+except IndexError: WIDTH = 3
 
 game = Game(
   p1=Player(X),
   p2=Computer(O),
   width=WIDTH,
-  limit=round(pow(WIDTH, EXPONENT)),
+  limit=round(pow(WIDTH, 1 if WIDTH == 4 else 2)),
   prune=True)
 
 print("\nTic-Tac-Toe\n")
 winner = game.play()
-print("Tie" if not winner else f"Winner {winner}")
+print(f"{winner} wins" if winner else "Tie")
